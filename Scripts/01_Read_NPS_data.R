27ae47386c2b5c07dcac36a20d9c9e4e35360492
charles-cunningham <45041242+charles-cunningham@users.noreply.github.com>
2025-05-16 14:53:44 +0100

    Filter out titles intersecting Wales

--- Scripts/01_Read_NPS_data.R
+++ Scripts/01_Read_NPS_data.R
@@ -6,9 +6,10 @@
 #
 # Script Description: Reads in the NPS data (which comprises several parts which
 # cannot all be read in at the same time due to large memory requirements), 
-# then create a data frame containing title number area. This is used to 
-# filter title numbers (which can comprise of multiple polygons across different
-# part of the NPS polygon data). The filtered title numbers are used to select 
+# then create a data frame containing title number area and whether polygon is
+# is within or touches England and is outside Wales. This is used to 
+# filter title numbers (which can comprise multiple polygons across different
+# parts of the NPS polygon data). The filtered title numbers are used to select 
 # polygons from different parts, which are then merged together and saved.
 
 ### LOAD LIBRARIES -------------------------------------------------------------
@@ -35,6 +36,29 @@ dataDir <- "/dbfs/mnt/lab/unrestricted/charles.cunningham@defra.gov.uk/LandRegis
 if (!file.exists(paste0(dataDir, "NPS_data/Raw"))) {
   dir.create(paste0(dataDir, "NPS_data/Raw"), recursive = TRUE)
 }
+
+### DOWNLOAD COUNTRY BOUNDARY DATA [MANUAL] ------------------------------------
+
+# We use the Countries (December 2024) Boundaries UK BGC data from ONS
+# (Boundary, Super Generalised Clipped)
+
+# Information on data here, and can also download the .geojson file in Download tab:
+# https://geoportal.statistics.gov.uk/datasets/6f18dfc308d04372929dea6afa44b2c7_0/explore
+
+# Once downloaded, file is moved to dataDir
+
+### PROCESS COUNTRY DATA -------------------------------------------------------
+
+# Read UK country boundaries
+boundaryUK <- paste0(dataDir,
+                     "Countries_December_2024_Boundaries_UK_BSC_-2880276037664496196.geojson") %>%
+  read_sf(.) %>%
+  .["CTRY24NM"] %>%
+  st_transform(., crs = 27700)
+
+# Create country boundaries
+england <- filter(boundaryUK, CTRY24NM == "England")
+wales <- filter(boundaryUK, CTRY24NM == "Wales")
 
 ### CONNECT TO LAND REGISTRY DATA ----------------------------------------------
 
@@ -76,7 +100,7 @@ lapply(NPS_files, function(x) {
                  )
 })
 
-### CREATE MERGED NPS TIBBLE WITH POLY_ID, TITLE_NO AND AREA COLUMNS -----------
+### CREATE MERGED NPS TIBBLE WITH ADDED TIBBLE AREA & COUNTRY COLUMNS ----------
 
 # Create empty tibble
 NPS_df <- tibble()
@@ -94,15 +118,21 @@ for(i in NPS_shp) {
   print(paste("Processing:", basename(i)))
 
   # Read in shapefile
-  NPS_part <- st_read(i, quiet = TRUE)
-  
+  NPS_part <- read_sf(i)
+
   # Add polygon area column
-  NPS_part$POLY_AREA <- st_area(NPS_part) 
+  NPS_part$POLY_AREA <- st_area(NPS_part)
   
+  # Check if polygon is within or touches England (intersects)
+  NPS_part$inEngland <- st_intersects(NPS_part, england, sparse = FALSE)[,1]
+  
+  # Check if polygon is outside Wales (does not intersect)
+  NPS_part$outWales <- st_disjoint(NPS_part, wales, sparse = FALSE)[,1]
+
   # Convert NPS_part to tibble with POLY_ID, TITLE_NO, and POLY_AREA columns
   NPS_part_df <- NPS_part %>%
     as_tibble() %>%
-    select(POLY_ID, TITLE_NO, POLY_AREA)
+    select(POLY_ID, TITLE_NO, POLY_AREA, inEngland, outWales)
   
   # Add rows to cumulative NPS tibble
   NPS_df <- rbind(NPS_df, NPS_part_df)
@@ -114,16 +144,25 @@ gc()
 
 ### CALCULATE AREA BY TITLE NUMBER AND FILTER ----------------------------------
 
-# Create title number area column
+# Create title summaries of area and suitable location
 NPS_df <- NPS_df %>%
-  group_by(TITLE_NO) %>% # Group polygons to title numbers
-  mutate(TITLE_AREA = as.numeric(POLY_AREA) %>% sum()) %>% # Sum polygons for each title number
-  ungroup() # Ungroup back
+  # Group polygons to titles, so for every polygon for each title number...
+  group_by(TITLE_NO) %>% 
+  # Sum polygon area
+  mutate(TITLE_AREA = as.numeric(POLY_AREA) %>% sum()) %>% 
+  # Create column whether title number intersects with England and not Wales 
+  mutate(TITLE_ENGLAND = all(inEngland) & all(outWales)) %>%
+  # Ungroup back
+  ungroup()
 
 # Create filtered vector of title numbers
 titleFilt <- NPS_df %>%
-  filter(TITLE_AREA >= minArea) %>% # Filter above minimum area
-  .[["TITLE_NO"]] # Select title numbers as vector
+  # Filter above minimum area
+  filter(TITLE_AREA >= minArea) %>%
+  # Filter location that intersects with England but outside Wales
+  filter(TITLE_ENGLAND == TRUE) %>%
+  # Select title numbers as vector
+  .[["TITLE_NO"]]
 
 ### FILTER AND MERGE NPS POLYGONS ----------------------------------------------
 
